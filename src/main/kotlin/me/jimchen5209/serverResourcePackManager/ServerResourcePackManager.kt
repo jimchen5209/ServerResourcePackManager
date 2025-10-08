5ad790a5e60c07cf158fd04b2ca012e770476f8d
jimchen5209 <jimchen5209@gmail.com>
2025-10-08 13:49:58 +0800

    feat: initial commit

---
+++ src/main/kotlin/me/jimchen5209/serverResourcePackManager/ServerResourcePackManager.kt
@@ -0,0 +1,10 @@
+package me.jimchen5209.serverResourcePackManager
+
+import net.fabricmc.api.ModInitializer
+
+class ServerResourcePackManager : ModInitializer {
+
+    override fun onInitialize() {
+
+    }
+}
